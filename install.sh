3162a117d21fb378fe13cddc527da73d221ab7cb
Arseny <a.yanchenko@expasoft.tech>
2021-09-09 14:47:30 +0700

    feat: support Jetson packages in install.sh

--- install.sh
+++ install.sh
@@ -3,29 +3,78 @@ set -e -u
 
 function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
 
-ORT_PY37_WHL_URL="https://github.com/ENOT-AutoDL/ONNX-Runtime-with-TensorRT-and-OpenVINO/releases/download/v1.8.1_with_embedded_deps/onnxruntime_gpu_tensorrt-1.8.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
-ORT_PY38_WHL_URL="https://github.com/ENOT-AutoDL/ONNX-Runtime-with-TensorRT-and-OpenVINO/releases/download/v1.8.1_with_embedded_deps/onnxruntime_gpu_tensorrt-1.8.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
+BASE_URL="https://github.com/ENOT-AutoDL/ONNX-Runtime-with-TensorRT-and-OpenVINO/releases/download/"
+ORT_PY37_WHL_URL="${BASE_URL}v1.8.1_with_embedded_deps/onnxruntime_gpu_tensorrt-1.8.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
+ORT_PY38_WHL_URL="${BASE_URL}v1.8.1_with_embedded_deps/onnxruntime_gpu_tensorrt-1.8.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
+ORT_PY36_AARCH64_JP46_WHL_URL="${BASE_URL}v1.8.2_JetPack4.6/onnxruntime_gpu_tensorrt-1.8.2-cp36-cp36m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+ORT_PY37_AARCH64_JP46_WHL_URL="${BASE_URL}v1.8.2_JetPack4.6/onnxruntime_gpu_tensorrt-1.8.2-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+ORT_PY38_AARCH64_JP46_WHL_URL="${BASE_URL}v1.8.2_JetPack4.6/onnxruntime_gpu_tensorrt-1.8.2-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+ORT_PY36_AARCH64_JP45_WHL_URL="${BASE_URL}v1.8.2_JetPack4.5/onnxruntime_gpu_tensorrt-1.8.2-cp36-cp36m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+ORT_PY37_AARCH64_JP45_WHL_URL="${BASE_URL}v1.8.2_JetPack4.5/onnxruntime_gpu_tensorrt-1.8.2-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+ORT_PY38_AARCH64_JP45_WHL_URL="${BASE_URL}v1.8.2_JetPack4.5/onnxruntime_gpu_tensorrt-1.8.2-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
 
-driver_version="$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)"
-minimal_driver_version='460.27.04'
-
-if [[ $(ver "$driver_version") < $(ver "$minimal_driver_version") ]]; then
-    printf "NVIDIA driver version should be %s or higher.\n" "$minimal_driver_version"
-    printf "Current NVIDIA driver version %s. Abort.\n" "$driver_version"
-    exit 1
-fi
-
+arch="$(uname -m)"
 python_version="$(python -c 'import platform; print(platform.python_version())')"
-if ! [[ "$python_version" =~ 3\.[7-8]\.* ]]; then
-    printf "Unsupported python version. Abort.\n"
-    exit 1
-fi
 
-python -m pip install -U pip
-pip install wheel
+if [[ $arch == "x86_64" ]]; then
 
-if [[ $python_version == "3.7"* ]]; then
-    pip install $ORT_PY37_WHL_URL --extra-index-url https://pypi.ngc.nvidia.com
-elif [[ $python_version == "3.8"* ]]; then
-    pip install $ORT_PY38_WHL_URL --extra-index-url https://pypi.ngc.nvidia.com
+    driver_version="$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)"
+    minimal_driver_version='460.27.04'
+    
+    if [[ $(ver "$driver_version") < $(ver "$minimal_driver_version") ]]; then
+        printf "NVIDIA driver version should be %s or higher.\n" "$minimal_driver_version"
+        printf "Current NVIDIA driver version %s. Abort.\n" "$driver_version"
+        exit 1
+    fi
+    
+    if ! [[ "$python_version" =~ 3\.[7-8]\.* ]]; then
+        printf "Unsupported python version. Abort.\n"
+        exit 1
+    fi
+    
+    python -m pip install -U pip
+    pip install wheel
+    
+    if [[ $python_version == "3.7"* ]]; then
+        pip install $ORT_PY37_WHL_URL --extra-index-url https://pypi.ngc.nvidia.com
+    elif [[ $python_version == "3.8"* ]]; then
+        pip install $ORT_PY38_WHL_URL --extra-index-url https://pypi.ngc.nvidia.com
+    fi
+
+elif [[ $arch == "aarch64" ]]; then
+    
+    declare -A supported_jetpacks=(["6.1"]="JetPack 4.6" ["5.1"]="JetPack 4.5")
+    latest_jetpack="6.1"
+    jetpack_revision="$(cat /etc/nv_tegra_release | sed 's/.*REVISION: \([^,]*\).*/\1/')"
+
+    if ! [[ "$python_version" =~ 3\.[6-8]\.* ]]; then
+        printf "Unsupported python version. Abort.\n"
+        exit 1
+    fi
+
+    if [[ $jetpack_revision == "6.1" ]]; then
+        if [[ $python_version == "3.6"* ]]; then
+            pip install $ORT_PY36_AARCH64_JP46_WHL_URL
+	elif [[ $python_version == "3.7"* ]]; then
+            pip install $ORT_PY37_AARCH64_JP46_WHL_URL
+	elif [[ $python_version == "3.8"* ]]; then
+            pip install $ORT_PY38_AARCH64_JP46_WHL_URL
+	fi
+    elif [[ $jetpack_revision == "5.1" ]]; then
+        if [[ $python_version == "3.6"* ]]; then
+            pip install $ORT_PY36_AARCH64_JP45_WHL_URL
+	elif [[ $python_version == "3.7"* ]]; then
+            pip install $ORT_PY37_AARCH64_JP45_WHL_URL
+	elif [[ $python_version == "3.8"* ]]; then
+            pip install $ORT_PY38_AARCH64_JP45_WHL_URL
+	fi
+        printf "Please update installed ${supported_jetpacks[$jetpack_revision]} to ${supported_jetpacks[$latest_jetpack]}.\n"
+    else
+        printf "Unsupported JetPack. Abort.\n"
+        exit 1
+    fi
+
+else
+    printf "Unsupported architecture. Abort.\n"
+        exit 1
 fi
