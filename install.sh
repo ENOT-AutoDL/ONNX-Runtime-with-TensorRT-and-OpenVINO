6c0728809542d9f7eea6c604f41f8b7be2c0caa8
Arseny Yanchenko <a.yanchenko@expasoft.tech>
2021-10-15 15:55:50 +0700

    feat: update install.sh for x86_64 arch (and chores)

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@ set -e -u
 
 function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
 
-BASE_URL="https://github.com/ENOT-AutoDL/ONNX-Runtime-with-TensorRT-and-OpenVINO/releases/download/"
-ORT_PY37_WHL_URL="${BASE_URL}v1.8.1_with_embedded_deps/onnxruntime_gpu_tensorrt-1.8.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
-ORT_PY38_WHL_URL="${BASE_URL}v1.8.1_with_embedded_deps/onnxruntime_gpu_tensorrt-1.8.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
-ORT_PY36_AARCH64_JP46_WHL_URL="${BASE_URL}v1.8.2_JetPack4.6/onnxruntime_gpu_tensorrt-1.8.2-cp36-cp36m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
-ORT_PY37_AARCH64_JP46_WHL_URL="${BASE_URL}v1.8.2_JetPack4.6/onnxruntime_gpu_tensorrt-1.8.2-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
-ORT_PY38_AARCH64_JP46_WHL_URL="${BASE_URL}v1.8.2_JetPack4.6/onnxruntime_gpu_tensorrt-1.8.2-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
-ORT_PY36_AARCH64_JP45_WHL_URL="${BASE_URL}v1.8.2_JetPack4.5/onnxruntime_gpu_tensorrt-1.8.2-cp36-cp36m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
-ORT_PY37_AARCH64_JP45_WHL_URL="${BASE_URL}v1.8.2_JetPack4.5/onnxruntime_gpu_tensorrt-1.8.2-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
-ORT_PY38_AARCH64_JP45_WHL_URL="${BASE_URL}v1.8.2_JetPack4.5/onnxruntime_gpu_tensorrt-1.8.2-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+BASE_URL="https://github.com/ENOT-AutoDL/ONNX-Runtime-with-TensorRT-and-OpenVINO/releases/download"
+ORT_PY37_WHL_URL="${BASE_URL}/v1.9.0/onnxruntime_gpu_tensorrt-1.9.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
+ORT_PY38_WHL_URL="${BASE_URL}/v1.9.0/onnxruntime_gpu_tensorrt-1.9.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
+ORT_PY36_AARCH64_JP46_WHL_URL="${BASE_URL}/v1.8.2_JetPack4.6/onnxruntime_gpu_tensorrt-1.8.2-cp36-cp36m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+ORT_PY37_AARCH64_JP46_WHL_URL="${BASE_URL}/v1.8.2_JetPack4.6/onnxruntime_gpu_tensorrt-1.8.2-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+ORT_PY38_AARCH64_JP46_WHL_URL="${BASE_URL}/v1.8.2_JetPack4.6/onnxruntime_gpu_tensorrt-1.8.2-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+ORT_PY36_AARCH64_JP45_WHL_URL="${BASE_URL}/v1.8.2_JetPack4.5/onnxruntime_gpu_tensorrt-1.8.2-cp36-cp36m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+ORT_PY37_AARCH64_JP45_WHL_URL="${BASE_URL}/v1.8.2_JetPack4.5/onnxruntime_gpu_tensorrt-1.8.2-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+ORT_PY38_AARCH64_JP45_WHL_URL="${BASE_URL}/v1.8.2_JetPack4.5/onnxruntime_gpu_tensorrt-1.8.2-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
 
 arch="$(uname -m)"
 python_version="$(python -c 'import platform; print(platform.python_version())')"
@@ -20,21 +20,21 @@ if [[ $arch == "x86_64" ]]; then
 
     driver_version="$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)"
     minimal_driver_version='460.27.04'
-    
+
     if [[ $(ver "$driver_version") < $(ver "$minimal_driver_version") ]]; then
         printf "NVIDIA driver version should be %s or higher.\n" "$minimal_driver_version"
         printf "Current NVIDIA driver version %s. Abort.\n" "$driver_version"
         exit 1
     fi
-    
+
     if ! [[ "$python_version" =~ 3\.[7-8]\.* ]]; then
         printf "Unsupported python version. Abort.\n"
         exit 1
     fi
-    
+
     python -m pip install -U pip
     pip install wheel
-    
+
     if [[ $python_version == "3.7"* ]]; then
         pip install $ORT_PY37_WHL_URL --extra-index-url https://pypi.ngc.nvidia.com
     elif [[ $python_version == "3.8"* ]]; then
@@ -42,7 +42,7 @@ if [[ $arch == "x86_64" ]]; then
     fi
 
 elif [[ $arch == "aarch64" ]]; then
-    
+
     declare -A supported_jetpacks=(["6.1"]="JetPack 4.6" ["5.1"]="JetPack 4.5")
     latest_jetpack="6.1"
     jetpack_revision="$(cat /etc/nv_tegra_release | sed 's/.*REVISION: \([^,]*\).*/\1/')"
@@ -51,6 +51,8 @@ elif [[ $arch == "aarch64" ]]; then
         printf "Unsupported python version. Abort.\n"
         exit 1
     fi
+
+    python -m pip install -U pip
 
     if [[ $jetpack_revision == "6.1" ]]; then
         if [[ $python_version == "3.6"* ]]; then
